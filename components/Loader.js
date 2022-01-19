1f12b670af8fad0d5ef4d0afc781c85cdfbb4221
wb505 <82988726+wb505@users.noreply.github.com>
2022-01-19 14:27:13 +0600

    Cng

--- components/Loader.js
+++ components/Loader.js
@@ -3,7 +3,7 @@
 const Loader = () => {
   return (
     <div className="App">
-        <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     </div>
   );
 }
